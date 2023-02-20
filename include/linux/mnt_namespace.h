8b1897b193e93ae7b3f66ba5725d064590d697a2
Alexander Mikhalitsyn <aleksandr.mikhalitsyn@canonical.com>
2023-02-20 15:25:38 +0100

    namespace: add sb_revalidate_bindmounts helper
    Useful if for some reason bindmounts root dentries get invalidated
    but it's needed to revalidate existing bindmounts without remounting.
    
    Signed-off-by: Alexander Mikhalitsyn <aleksandr.mikhalitsyn@canonical.com>

--- include/linux/mnt_namespace.h
+++ include/linux/mnt_namespace.h
@@ -3,6 +3,7 @@
 #define _NAMESPACE_H_
 #ifdef __KERNEL__
 
+struct super_block;
 struct mnt_namespace;
 struct fs_struct;
 struct user_namespace;
@@ -12,6 +13,8 @@ extern struct mnt_namespace *copy_mnt_ns(unsigned long, struct mnt_namespace *,
 		struct user_namespace *, struct fs_struct *);
 extern void put_mnt_ns(struct mnt_namespace *ns);
 extern struct ns_common *from_mnt_ns(struct mnt_namespace *);
+
+extern int sb_revalidate_bindmounts(struct super_block *sb);
 
 extern const struct file_operations proc_mounts_operations;
 extern const struct file_operations proc_mountinfo_operations;
