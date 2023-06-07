5c129cf122be569f776f66974b3626e4cebc44f3
Alexander Mikhalitsyn <aleksandr.mikhalitsyn@canonical.com>
2023-06-07 16:25:51 +0200

    ceph: pass idmap to __ceph_setattr
    Just pass down the mount's idmapping to __ceph_setattr,
    because we will need it later.
    
    Cc: Xiubo Li <xiubli@redhat.com>
    Cc: Jeff Layton <jlayton@kernel.org>
    Cc: Ilya Dryomov <idryomov@gmail.com>
    Cc: brauner@kernel.org
    Cc: ceph-devel@vger.kernel.org
    Signed-off-by: Alexander Mikhalitsyn <aleksandr.mikhalitsyn@canonical.com>

--- fs/ceph/inode.c
+++ fs/ceph/inode.c
@@ -2424,7 +2424,8 @@ out:
 	return ret;
 }
 
-int __ceph_setattr(struct inode *inode, struct iattr *attr, struct ceph_iattr *cia)
+int __ceph_setattr(struct mnt_idmap *idmap, struct inode *inode,
+		   struct iattr *attr, struct ceph_iattr *cia)
 {
 	struct ceph_inode_info *ci = ceph_inode(inode);
 	unsigned int ia_valid = attr->ia_valid;
@@ -2695,6 +2696,7 @@ retry:
 	if (mask) {
 		req->r_inode = inode;
 		ihold(inode);
+		req->r_mnt_idmap = mnt_idmap_get(idmap);
 		req->r_inode_drop = release;
 		req->r_args.setattr.mask = cpu_to_le32(mask);
 		req->r_num_caps = 1;
@@ -2766,7 +2768,7 @@ int ceph_setattr(struct mnt_idmap *idmap, struct dentry *dentry,
 	    ceph_quota_is_max_bytes_exceeded(inode, attr->ia_size))
 		return -EDQUOT;
 
-	err = __ceph_setattr(inode, attr, NULL);
+	err = __ceph_setattr(idmap, inode, attr, NULL);
 
 	if (err >= 0 && (attr->ia_valid & ATTR_MODE))
 		err = posix_acl_chmod(&nop_mnt_idmap, dentry, attr->ia_mode);
